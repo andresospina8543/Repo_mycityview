0065df1908c23e99bf30533d51022fe3ec822fd2
ANDRESD <davidneopunk@gmail.com>
2015-08-23 11:20:17 -0500

    Inclusion GooglePolyline para el json de google

--- MyCityView/src/co/com/mycityview/model/routes/Step.java
+++ MyCityView/src/co/com/mycityview/model/routes/Step.java
@@ -3,6 +3,16 @@ package co.com.mycityview.model.routes;
 public class Step {
 	private Location end_location;
 	private Location start_location;
+	private GooglePolyline polyline;
+
+	public GooglePolyline getPolyline() {
+		return polyline;
+	}
+
+	public void setPolyline(GooglePolyline polyline) {
+		this.polyline = polyline;
+	}
+
 	public Location getEnd_location() {
 		return end_location;
 	}
